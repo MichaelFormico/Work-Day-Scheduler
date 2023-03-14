54af7f3b531e0b3c4292583dcfa135c11804a60f
Furly <michaelformico@gmail.com>
2023-03-14 14:20:06 -0700

    ch ch ch changes

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ let future = document.createElement('future')
 
 
     let hour = dayjs().format('HH')
+    console.log(hour)
     let timeHours = document.querySelectorAll(".time-block")
 
     for(let i = 0; i < timeHours.length; i++) {
@@ -33,10 +34,29 @@ let saveButton = document.getElementById('save')
 
 let comments = document.getElementById('comment')
 
-saveButton.addEventListener("click", function() {
-    localStorage.setItem("comments", JSON.stringify(comments));
-    console.log(comments)
+$(".saveBtn").on("click", function() {
+    var divID = $(this).parent().attr("id")
+    var text = $(this).siblings(".description").val()
+
+    localStorage.setItem(divID, text)
 })
+
+$("#09 .description").val(localStorage.getItem("09"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+
+
+
+// saveButton.addEventListener("click", function() {
+//     localStorage.setItem("comments", JSON.stringify(comments));
+//     console.log(comments)
+// })
 
 
 
